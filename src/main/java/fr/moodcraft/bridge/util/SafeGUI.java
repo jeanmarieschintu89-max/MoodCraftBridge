c7e646827eb22f0568ffc623915417cda724bee8
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-05 19:51:54 +0200

    Update SafeGUI.java

--- src/main/java/fr/moodcraft/bridge/util/SafeGUI.java
+++ src/main/java/fr/moodcraft/bridge/util/SafeGUI.java
@@ -83,7 +83,7 @@ public class SafeGUI {
                 ItemFlag.HIDE_UNBREAKABLE,
                 ItemFlag.HIDE_DESTROYS,
                 ItemFlag.HIDE_PLACED_ON,
-                ItemFlag.HIDE_POTION_EFFECTS
+                ItemFlag.HIDE_ADDITIONAL_TOOLTIP
         );
     }
 
