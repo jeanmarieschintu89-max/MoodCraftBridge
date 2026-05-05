9cd6f859ce5ccda25481f3db6c8be712c2ffd417
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-05 18:50:13 +0200

    Update ReputationManager.java

--- src/main/java/fr/moodcraft/bridge/manager/ReputationManager.java
+++ src/main/java/fr/moodcraft/bridge/manager/ReputationManager.java
@@ -1,26 +1,8 @@
-package fr.moodcraft.bridge.listener;
+package fr.moodcraft.bridge.util;
 
-import fr.moodcraft.bridge.util.ReputationManager;
+public class ReputationManager {
 
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
-
-public class ChatListener implements Listener {
-
-    @EventHandler(priority = EventPriority.HIGH)
-    public void chat(AsyncPlayerChatEvent e) {
-
-        if (e.isCancelled()) return;
-
-        var p = e.getPlayer();
-        String player = p.getName();
-        String message = e.getMessage();
-
-        String rep = ReputationManager.format(player);
-        if (rep == null) rep = "§7?";
-
-        e.setFormat("§7[" + rep + "] §f" + player + " §8» §f" + message);
+    public static String format(String player) {
+        return "§a100";
     }
 }
