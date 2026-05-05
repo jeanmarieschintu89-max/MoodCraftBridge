497828574b72446513c8c95263ba29e65301b01d
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-05 03:12:08 +0200

    Update EcoReloadCommand.java

--- src/main/java/fr/moodcraft/bridge/EcoReloadCommand.java
+++ src/main/java/fr/moodcraft/bridge/EcoReloadCommand.java
@@ -9,13 +9,10 @@ public class EcoReloadCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
-        // 🔥 reload market propre
         MarketAPI.reload();
 
-        // 🔄 rebuild shops
         ShopIndex.rebuild();
 
-        // 🔄 sync prix
         for (String item : MarketAPI.getItems()) {
             PriceUpdater.updateItem(item);
         }
