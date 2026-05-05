751930c622367d5918c25a95e0c7df6e0363608b
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-05 18:29:09 +0200

    Create ReputationManager.java

---
+++ src/main/java/fr/moodcraft/bridge/manager/ReputationManager.java
@@ -0,0 +1,10 @@
+package fr.moodcraft.bridge.util;
+
+public class ReputationManager {
+
+    // 🔹 format affichage
+    public static String format(String player) {
+        // 👉 système simple temporaire
+        return "§a100"; // valeur fixe (tu pourras brancher un vrai système après)
+    }
+}
