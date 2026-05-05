0d4270235bb942d57b417ad1358ac307fe3c8bca
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-05 20:39:39 +0200

    Update Main.java

--- src/main/java/fr/moodcraft/bridge/Main.java
+++ src/main/java/fr/moodcraft/bridge/Main.java
@@ -1,6 +1,7 @@
 package fr.moodcraft.bridge;
 
 import fr.moodcraft.bridge.bank.BankStorage;
+import fr.moodcraft.bridge.command.BanqueCommand;
 import fr.moodcraft.bridge.command.*;
 import fr.moodcraft.bridge.gui.*;
 import fr.moodcraft.bridge.handler.*;
