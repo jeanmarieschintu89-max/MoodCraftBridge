debb4bae524c416972e72d893f0b691864b9ec07
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-05 14:04:10 +0200

    Create TransferTargetGUI.java

---
+++ src/main/java/fr/moodcraft/bridge/gui/TransferTargetGUI.java
@@ -0,0 +1,36 @@
+package fr.moodcraft.bridge.gui;
+
+import fr.moodcraft.bridge.manager.GUIManager;
+import fr.moodcraft.bridge.gui.SafeGUI;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+
+public class TransferTargetGUI {
+
+    public static void open(Player p) {
+
+        // 🔥 titre aligné avec ton listener
+        Inventory inv = Bukkit.createInventory(null, 27, "§eChoisir joueur virement");
+
+        int slot = 0;
+
+        for (Player target : Bukkit.getOnlinePlayers()) {
+
+            if (target.equals(p)) continue;
+            if (slot >= 27) break;
+
+            SafeGUI.safeSet(inv, slot,
+                    SafeGUI.item(Material.PLAYER_HEAD,
+                            "§a" + target.getName(),
+                            "§8────────",
+                            "§7Cliquer pour sélectionner"));
+
+            slot++;
+        }
+
+        GUIManager.open(p, "transfer_target", inv);
+    }
+}
