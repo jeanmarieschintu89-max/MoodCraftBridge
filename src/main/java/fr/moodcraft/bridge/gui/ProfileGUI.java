dda3d025fa5b89f1b0219e2642eff4f7c645ea63
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-09 22:10:23 +0200

    Update ProfileGUI.java

--- src/main/java/fr/moodcraft/bridge/gui/ProfileGUI.java
+++ src/main/java/fr/moodcraft/bridge/gui/ProfileGUI.java
@@ -236,9 +236,14 @@ public class ProfileGUI {
             );
 
             flagMeta.addItemFlags(
-                    ItemFlag.HIDE_ADDITIONAL_TOOLTIP,
-                    ItemFlag.HIDE_ATTRIBUTES,
-                    ItemFlag.HIDE_ENCHANTS
+        ItemFlag.HIDE_ATTRIBUTES,
+        ItemFlag.HIDE_ENCHANTS,
+        ItemFlag.HIDE_UNBREAKABLE,
+        ItemFlag.HIDE_DESTROYS,
+        ItemFlag.HIDE_PLACED_ON,
+        ItemFlag.HIDE_ITEM_SPECIFICS,
+        ItemFlag.HIDE_ADDITIONAL_TOOLTIP
+);
             );
 
             flag.setItemMeta(
