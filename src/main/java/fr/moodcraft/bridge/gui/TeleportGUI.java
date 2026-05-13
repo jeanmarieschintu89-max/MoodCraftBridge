f3139b55626552ce91ee1059bc76fe72bf44cd85
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-13 11:56:41 +0200

    Update TeleportGUI.java

--- src/main/java/fr/moodcraft/bridge/gui/TeleportGUI.java
+++ src/main/java/fr/moodcraft/bridge/gui/TeleportGUI.java
@@ -18,7 +18,7 @@ public class TeleportGUI {
                 Bukkit.createInventory(
                         null,
                         36,
-                        "§8✦ §6Téléportation §aMood§6Craft"
+                        "§8✦ §6Téléportation §aMood§6Craft §8✦"
                 );
 
         SafeGUI.fill(
