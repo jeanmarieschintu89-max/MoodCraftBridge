3ceb1846d6d93dffcf97a2cca8556753e6275817
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-13 12:40:53 +0200

    Update PrixCommand.java

--- src/main/java/fr/moodcraft/bridge/command/PrixCommand.java
+++ src/main/java/fr/moodcraft/bridge/command/PrixCommand.java
@@ -1,16 +1,29 @@
 package fr.moodcraft.bridge.command;
 
 import fr.moodcraft.bridge.gui.PriceGUI;
-import org.bukkit.command.*;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
 import org.bukkit.entity.Player;
 
 public class PrixCommand implements CommandExecutor {
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    public boolean onCommand(
+            CommandSender sender,
+            Command command,
+            String label,
+            String[] args
+    ) {
 
         if (!(sender instanceof Player p)) {
-            sender.sendMessage("§cCommande joueur uniquement.");
+
+            sender.sendMessage(
+                    "§cCommande joueur uniquement."
+            );
+
             return true;
         }
 
