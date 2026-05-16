ba088cb0b4702f78559a2fd4253908edcc433f01
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-16 14:17:15 +0200

    fix: set minigames teleport button to warp jeux

--- src/main/java/fr/moodcraft/bridge/handler/TeleportHandler.java
+++ src/main/java/fr/moodcraft/bridge/handler/TeleportHandler.java
@@ -51,10 +51,10 @@ public class TeleportHandler implements GUIHandler {
 
             case 14 -> teleport(
                     p,
-                    "warp minijeux",
+                    "warp jeux",
                     false,
-                    "§d✦ Mini jeux",
-                    "§7Chargement de la zone d'activités.",
+                    "§d✦ Mini-jeux",
+                    "§7Chargement de la zone jeux.",
                     Sound.ENTITY_ALLAY_AMBIENT_WITHOUT_ITEM,
                     1.15f,
                     Sound.BLOCK_AMETHYST_BLOCK_CHIME,
