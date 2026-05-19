edafba941948f8c35bb2d2eeb3f17c08eee2eb87
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-19 23:29:56 +0200

    Restore emerald to QuickShop market sync

--- src/main/java/fr/moodcraft/bridge/manager/PriceUpdater.java
+++ src/main/java/fr/moodcraft/bridge/manager/PriceUpdater.java
@@ -14,6 +14,7 @@ public final class PriceUpdater {
 
     static {
         ALLOWED.add("diamond");
+        ALLOWED.add("emerald");
         ALLOWED.add("gold");
         ALLOWED.add("iron");
         ALLOWED.add("copper");
